0758c1391ab994fce1ab0f1913fbd80b663190f7
SNQ-0 <taishin05109@gmail.com>
2022-04-17 23:26:32 +0900

    Create package

--- Tests/ReviewRequestSwiftUITests/ReviewRequestSwiftUITests.swift
+++ Tests/ReviewRequestSwiftUITests/ReviewRequestSwiftUITests.swift
@@ -6,6 +6,6 @@ final class ReviewRequestSwiftUITests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssert(true, "Pass")
+        XCTAssert(true, "TEST")
     }
 }
